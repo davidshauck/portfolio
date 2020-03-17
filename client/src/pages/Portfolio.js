e7fdb9a8c63ba1c349bb268f4c398859c8f7cbba
David Hauck <dhauck@mac.com>
2020-03-17 11:07:33 -0400

    added 3 new portfolio items and make links open in target tabs

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -5,6 +5,10 @@ import CrystalCountdown from '../images/crystal_countdown.png'
 import JavascriptJeopardy from '../images/javascript_jeopardy.png'
 import NapkinHangman from '../images/napkin_hangman.png'
 import Codehort from '../images/codehort.png'
+import ClickyGame from '../images/clicky_game.png'
+import GifHorse from '../images/gif_horse.png'
+import TrainScheduler from '../images/train_scheduler.png'
+
 
 function Portfolio() {
   const [index, setIndex] = useState(0);
@@ -17,7 +21,7 @@ function Portfolio() {
     <div className="carousel-container">
     <Carousel activeIndex={index} onSelect={handleSelect}>
     <Carousel.Item>
-      <a href="https://codehort.herokuapp.com/">
+      <a target="_blank" rel="noopener noreferrer" href="https://codehort.herokuapp.com/">
         <img
           className="d-block w-100 image"
           src={Codehort}
@@ -26,7 +30,7 @@ function Portfolio() {
       </a>
       </Carousel.Item>
       <Carousel.Item>
-        <a href="https://bananaknifefight.herokuapp.com/">
+        <a target="_blank" rel="noopener noreferrer" href="https://bananaknifefight.herokuapp.com/">
           <img
             className="d-block w-100 image"
             src={BananaKnifeFight}
@@ -35,7 +39,7 @@ function Portfolio() {
         </a>
       </Carousel.Item>
       <Carousel.Item>
-      <a href="https://davidshauck.github.io/Crystal-Collector/">
+      <a target="_blank" rel="noopener noreferrer" href="https://davidshauck.github.io/Crystal-Collector/">
         <img
           className="d-block w-100 image"
           src={CrystalCountdown}
@@ -53,10 +57,37 @@ function Portfolio() {
       </a>
       </Carousel.Item>
       <Carousel.Item>
-      <a href="https://davidshauck.github.io/Word-Guess-Game/">
+      <a target="_blank" rel="noopener noreferrer" href="https://davidshauck.github.io/Word-Guess-Game/">
         <img
-          className="d-block w-100 image-black-border"
+          className="d-block w-100 image"
           src={NapkinHangman}
+          alt="Third slide"
+        />
+      </a>
+      </Carousel.Item>
+      <Carousel.Item>
+      <a target="_blank" rel="noopener noreferrer" href="https://davidshauck.github.io/clicky-game-2/">
+        <img
+          className="d-block w-100 image"
+          src={ClickyGame}
+          alt="Third slide"
+        />
+      </a>
+      </Carousel.Item>
+      <Carousel.Item>
+      <a target="_blank" rel="noopener noreferrer" href="https://davidshauck.github.io/GifTastic/">
+        <img
+          className="d-block w-100 image"
+          src={GifHorse}
+          alt="Third slide"
+        />
+      </a>
+      </Carousel.Item>
+      <Carousel.Item>
+      <a target="_blank" rel="noopener noreferrer" href="https://davidshauck.github.io/Train-Scheduler/">
+        <img
+          className="d-block w-100 image"
+          src={TrainScheduler}
           alt="Third slide"
         />
       </a>
